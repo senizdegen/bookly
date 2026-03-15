e28c98a5b9e0118e33eb1dc97f33ca3f3da92547
Dosymzhan <do5ymzh4n@gmail.com>
2026-03-15 16:01:58 +0500

    init db

---
+++ src/books/book_data.py
@@ -0,0 +1,56 @@
+books = [
+    {
+        "id": 1,
+        "title": "Clean Code",
+        "author": "Robert C. Martin",
+        "publisher": "Prentice Hall",
+        "published_date": "2008-08-01",
+        "page_count": 464,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "The Pragmatic Programmer",
+        "author": "Andrew Hunt",
+        "publisher": "Addison-Wesley",
+        "published_date": "1999-10-30",
+        "page_count": 352,
+        "language": "English"
+    },
+    {
+        "id": 3,
+        "title": "Introduction to Algorithms",
+        "author": "Thomas H. Cormen",
+        "publisher": "MIT Press",
+        "published_date": "2009-07-31",
+        "page_count": 1312,
+        "language": "English"
+    },
+    {
+        "id": 4,
+        "title": "Design Patterns",
+        "author": "Erich Gamma",
+        "publisher": "Addison-Wesley",
+        "published_date": "1994-10-31",
+        "page_count": 395,
+        "language": "English"
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures in Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 928,
+        "language": "English"
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-08-01",
+        "page_count": 688,
+        "language": "English"
+    }
+]
